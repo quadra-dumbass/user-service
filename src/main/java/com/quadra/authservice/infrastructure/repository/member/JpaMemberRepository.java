2871f8310fe158d3eac4511aec9e47b3c5b91e0c
rymph <rymph0501@gmail.com>
2025-08-02 20:08:32 +0900

    test

--- src/main/java/com/quadra/authservice/infrastructure/repository/member/JpaMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quadra.authservice.infrastructure.repository.member;
-
-import com.quadra.authservice.infrastructure.repository.member.schema.MemberSchema;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaMemberRepository extends JpaRepository<MemberSchema, Long> {
-}
